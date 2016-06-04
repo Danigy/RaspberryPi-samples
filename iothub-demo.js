24e46a3ed5f639f2cdcd536aeca7d383c98b22d7
Michael Chi <michi@microsoft.com>
2016-06-04 17:06:13 +0800

    add cmd_takepic command
    allow cloud send a command to raspberry pi camera to take a picture

--- iothub-demo.js
+++ iothub-demo.js
@@ -1,22 +1,29 @@
 'use strict';
 
 var readConfig = require('read-config');
+var spawn = require("child_process").spawn;
 
 var clientFromConnectionString = require('azure-iot-device-amqp').clientFromConnectionString;
 var Message = require('azure-iot-device').Message;
 
 var config = readConfig('./config.json');
+console.log('config=' + JSON.stringify(config));
 
+//var connectionString = 'HostName={host}.azure-devices.net;DeviceId={id};SharedAccessKey={key}';
 var connectionString = config.iotHubConnectionString;
 
+console.log(connectionString);
 var client = clientFromConnectionString(connectionString);
 
 function printResultFor(op) {
     return function printResult(err, res) {
         if (err) console.log(op + ' error: ' + err.toString());
         if (res) console.log(op + ' status: ' + res.constructor.name);
+
     };
 }
+
+var child_process = null;
 
 var connectCallback = function (err) {
     if (err) {
@@ -25,12 +32,14 @@ var connectCallback = function (err) {
         console.log('Client connected');
         client.on('message', function (msg) {
             client.complete(msg, printResultFor('completed'));
-            //Body = msg.data = {"name":"cmd_takepic"}
             console.log('Id: ' + msg.messageId + ' Body: ' + msg.data);
             var cmd = JSON.parse(msg.data);
             console.log('cmd = ' + cmd.name);
-            switch (msg.name) {
+            switch (cmd.name) {
                 case 'cmd_takepic':
+                    child_process = spawn('raspistill',['-o','test.jpg']);
+                    console.log('taking picture......');
+                    client.complete(msg, printResultFor('completed'));
                     break;
                 case 'cmd_streaming':
                     break;
@@ -40,15 +49,14 @@ var connectCallback = function (err) {
         });
         // Create a message and send it to the IoT Hub every second
         setInterval(function () {
-            
+
             var windSpeed = 10 + (Math.random() * 4);
             var data = JSON.stringify({ deviceId: 'mydevice', windSpeed: windSpeed });
             var message = new Message(data);
             console.log("Sending message: " + message.getData());
             client.sendEvent(message, printResultFor('send'));
-            
+
         }, 1000);
     }
 };
-
 client.open(connectCallback);
